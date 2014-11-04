f46dcee5a9f15823e78725b864bee3bcd5d89522
Óscar Nájera <najera.oscar@gmail.com>
2014-11-04 23:51:40 +0100

    Setup tool for pypi package

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Installer Sphinx extension for gallery generator
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="sphinx_gallery",
+    description="Sphinx extension to automatically generate an examples gallery",
+    version="0.0.1",
+    packages=find_packages(),
+    url="https://github.com/sphinx-gallery/sphinx-gallery",
+    author="Óscar Nájera",
+    author_email='najera.oscar@gmail.com',
+    install_requires=['Sphinx']
+)
