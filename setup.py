07f2ce43cec1588356e4e7fdecab200e378754bf
Óscar Nájera <najera.oscar@gmail.com>
2014-12-10 22:09:56 +0100

    Try to pass travis test

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     url="https://github.com/sphinx-gallery/sphinx-gallery",
     author="Óscar Nájera",
     author_email='najera.oscar@gmail.com',
-    install_requires=['scipy', 'Sphinx', 'matplotlib', 'pillow', 'scikit-learn', 'numpy'],
+    install_requires=['Sphinx', 'matplotlib', 'pillow', 'scikit-learn'],
     setup_requires=['nose>=1.0']
 )
