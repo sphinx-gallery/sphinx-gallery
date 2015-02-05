84e8c0b5e310cacd802909bfcfd85d33353f78f1
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-02-05 11:49:05 +0100

    add math example

--- examples/plot_sin.py
+++ examples/plot_sin.py
@@ -5,7 +5,15 @@ Plotting simple sin function
 ============================
 
 A simple example of the plot of a sin function
-in order to test the autonomy of the gallery
+in order to test the autonomy of the gallery.
+
+To illustrate how to insert maths in example descriptions,
+it corresponds mathematically to the function:
+
+.. math::
+
+    x \\rightarrow \\sin(x)
+
 """
 
 # Code source: Óscar Nájera
