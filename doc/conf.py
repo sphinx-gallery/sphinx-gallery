6d2e37973510911ba82fce7ab204a3b2fad11608
Óscar Nájera <najera.oscar@gmail.com>
2015-04-08 13:24:50 +0200

    CSS to highlight links in source code within example gallery

--- doc/conf.py
+++ doc/conf.py
@@ -110,6 +110,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+def setup(app):
+    app.add_stylesheet('theme_override.css')
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
