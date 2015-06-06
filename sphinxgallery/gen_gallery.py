356c2d671b24b0293c28684237fae0a20dd3cd64
Óscar Nájera <najera.oscar@gmail.com>
2015-06-06 14:43:00 +0200

    adopt the path class

--- sphinxgallery/gen_gallery.py
+++ sphinxgallery/gen_gallery.py
@@ -9,6 +9,46 @@ from .gen_rst import generate_dir_rst
 from .docs_resolv import embed_code_links
 
 
+class Path(str):
+    """Path object for manipulating directory and file paths."""
+
+    def __init__(self, path):
+        super(Path, self).__init__(path)
+
+    @property
+    def isdir(self):
+        return os.path.isdir(self)
+
+    @property
+    def exists(self):
+        """Return True if path exists"""
+        return os.path.exists(self)
+
+    def pjoin(self, *args):
+        """Join paths. `p` prefix prevents confusion with string method."""
+        return self.__class__(os.path.join(self, *args))
+
+    def psplit(self):
+        """Split paths. `p` prefix prevents confusion with string method."""
+        return [self.__class__(p) for p in os.path.split(self)]
+
+    def makedirs(self):
+        if not self.exists:
+            os.makedirs(self)
+
+    def listdir(self):
+        return os.listdir(self)
+
+    def format(self, *args, **kwargs):
+        return self.__class__(super(Path, self).format(*args, **kwargs))
+
+    def __add__(self, other):
+        return self.__class__(super(Path, self).__add__(other))
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+
 def clean_gallery_out(build_dir):
     # Sphinx hack: sphinx copies generated images to the build directory
     #  each time the docs are made.  If the desired image name already
