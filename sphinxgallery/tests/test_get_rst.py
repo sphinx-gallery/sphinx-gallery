070ed930440213fe682e24fec3f6daa87748b058
Óscar Nájera <najera.oscar@gmail.com>
2015-06-14 01:44:46 +0200

    relative path to nosetest execution

--- sphinxgallery/tests/test_get_rst.py
+++ sphinxgallery/tests/test_get_rst.py
@@ -12,11 +12,9 @@ from nose.tools import assert_equals
 def test_split_code_and_text_blocks():
     """Test if a known example file gets properly split"""
 
-    blocks = sg.split_code_and_text_blocks('../../examples/just_code.py')
+    blocks = sg.split_code_and_text_blocks('examples/just_code.py')
 
     assert_equals(blocks[0][0], 'text')
     assert_equals(blocks[0][1], (2, 11))
     assert_equals(blocks[1][0], 'code')
     assert_equals(blocks[1][1], (11, 16))
-
-test_split_code_and_text_blocks()
