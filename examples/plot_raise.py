38f9117750e484d2c33f1b844e9d743d9c3f6918
Óscar Nájera <najera.oscar@gmail.com>
2015-12-17 15:27:22 +0100

    Improve fail example writing

--- examples/plot_raise.py
+++ examples/plot_raise.py
@@ -10,6 +10,13 @@ image stamp. Thus allowing easy identification in the gallery display.
 You also get the python traceback of the failed code block
 """
 
+iae
+
+###############################################################################
+# Sphinx gallery as it executes scripts by block will continue
+# evaluating the script after exceptions, but there is no warranty
+# figure ordering will continue to match block's code. Anyway when the
+# script is broken, you should try to fix it first.
 
 # Code source: Óscar Nájera
 # License: BSD 3 clause
@@ -19,8 +26,8 @@ import matplotlib.pyplot as plt
 
 plt.pcolormesh(np.random.randn(100, 100))
 
-###############################################################################
-# Sphinx gallery as it executes scripts by block will continue
-# evaluating the script after exceptions
 
-aeiou
+###############################################################################
+# Here is another error raising Block
+
+raise IOError
