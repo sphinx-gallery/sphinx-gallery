b662d6e428c0bcdd628fa6323b422ce40033b34d
Loïc Estève <loic.esteve@ymail.com>
2016-02-17 09:37:55 +0100

    FIX __name__ should be set to __main__ when exececuting an example
    Modified one of the gallery example to use a if __name__ == __main__ guard.
    Fix #100

--- examples/plot_exp.py
+++ examples/plot_exp.py
@@ -15,18 +15,22 @@ stacking multiple images.
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-1, 2, 100)
-y = np.exp(x)
 
-plt.figure()
-plt.plot(x, y)
-plt.xlabel('$x$')
-plt.ylabel('$\exp(x)$')
+def main():
+    x = np.linspace(-1, 2, 100)
+    y = np.exp(x)
 
-plt.figure()
-plt.plot(x, -np.exp(-x))
-plt.xlabel('$x$')
-plt.ylabel('$-\exp(-x)$')
+    plt.figure()
+    plt.plot(x, y)
+    plt.xlabel('$x$')
+    plt.ylabel('$\exp(x)$')
 
+    plt.figure()
+    plt.plot(x, -np.exp(-x))
+    plt.xlabel('$x$')
+    plt.ylabel('$-\exp(-x)$')
 
-plt.show()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
