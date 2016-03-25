b3e6c3aa2e7f9283cd67495b8790777527fa0a8b
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-25 11:50:12 -0700

    added an example of a file that is not run by docs

---
+++ examples/skip_this-function.py
@@ -0,0 +1,20 @@
+"""
+
+=======================
+This example is not run
+=======================
+
+The code in this example should not be run because we will exclude
+it from processing using a regular expression to ignore any file
+that begins with 'skip_'. We do this by setting
+``filename_pattern = '/(?!skip_)'``
+
+"""
+
+print("This output should not appear!")
+
+##################################################################
+# More text.
+#
+
+print("This output shouldn't appear either!!!")
