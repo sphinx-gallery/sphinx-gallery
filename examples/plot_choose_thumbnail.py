0c40140610e1781b4a536367f20d6dc22768acab
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-13 10:26:25 -0400

    fix colon in example file

--- examples/plot_choose_thumbnail.py
+++ examples/plot_choose_thumbnail.py
@@ -4,10 +4,10 @@
 Choosing the thumbnail plot
 ===========================
 
-An example to demonstrate how to choose which figure is displayed as the thumbnail
-if the example generates more than one figure. This is done by specifying the
-keyword-value pair ``sphinx_gallery_thumbnail_number: 2`` as a comment below the
-docstring in the example file.
+An example to demonstrate how to choose which figure is displayed as the
+thumbnail if the example generates more than one figure. This is done by
+specifying the keyword-value pair ``sphinx_gallery_thumbnail_number = 2`` as a
+comment somewherebelow the docstring in the example file.
 """
 
 # Code source: Óscar Nájera
