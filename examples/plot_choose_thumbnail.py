ab2af751b24348c13451d16e6b3c1ba5011d4048
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-14 08:37:50 -0400

    typo

--- examples/plot_choose_thumbnail.py
+++ examples/plot_choose_thumbnail.py
@@ -7,7 +7,7 @@ Choosing the thumbnail figure
 An example to demonstrate how to choose which figure is displayed as the
 thumbnail if the example generates more than one figure. This is done by
 specifying the keyword-value pair ``sphinx_gallery_thumbnail_number = 2`` as a
-comment somewherebelow the docstring in the example file.
+comment somewhere below the docstring in the example file.
 """
 
 # Code source: Óscar Nájera
