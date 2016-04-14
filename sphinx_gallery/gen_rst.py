9265fc035401581051e7bcc5bb17432c84abd76f
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-04-14 07:57:25 -0400

    change image path

--- sphinx_gallery/gen_rst.py
+++ sphinx_gallery/gen_rst.py
@@ -298,12 +298,11 @@ def check_md5sum_change(src_file):
     return src_file_changed
 
 
-def _plots_are_current(src_file, image_file):
+def _plots_are_current(src_file, image_path):
     """Test existence of image file and no change in md5sum of
     example"""
 
-    first_image_file = image_file.format(1)
-    has_image = os.path.exists(first_image_file)
+    has_image = os.path.exists(image_path)
     src_file_changed = check_md5sum_change(src_file)
 
     return has_image and not src_file_changed
@@ -405,18 +404,17 @@ def scale_image(in_fname, out_fname, max_width, max_height):
                           generated images')
 
 
-def save_thumbnail(image_path, base_image_name, gallery_conf, thumb_num=1):
+def save_thumbnail(thumbnail_image_path, base_image_name, gallery_conf):
     """Save the thumbnail image"""
-    image_file = image_path.format(thumb_num)
-    thumb_dir = os.path.join(os.path.dirname(image_file), 'thumb')
+    thumb_dir = os.path.join(os.path.dirname(thumbnail_image_path), 'thumb')
     if not os.path.exists(thumb_dir):
         os.makedirs(thumb_dir)
 
     thumb_file = os.path.join(thumb_dir,
                               'sphx_glr_%s_thumb.png' % base_image_name)
 
-    if os.path.exists(image_file):
-        scale_image(image_file, thumb_file, 400, 280)
+    if os.path.exists(thumbnail_image_path):
+        scale_image(thumbnail_image_path, thumb_file, 400, 280)
     elif not os.path.exists(thumb_file):
         # create something to replace the thumbnail
         default_thumb_file = os.path.join(glr_path_static(), 'no_image.png')
@@ -569,7 +567,7 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
 
     base_image_name = os.path.splitext(fname)[0]
     image_fname = 'sphx_glr_' + base_image_name + '_{0:03}.png'
-    image_path = os.path.join(image_dir, image_fname)
+    image_path_template = os.path.join(image_dir, image_fname)
 
     script_blocks = split_code_and_text_blocks(example_file)
 
@@ -581,7 +579,8 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
                           for blabel, bcontent in script_blocks
                           if blabel == 'code'])
 
-    if _plots_are_current(example_file, image_path):
+    first_image_path = image_path_template.format(1)
+    if _plots_are_current(example_file, first_image_path):
         return amount_of_code, 0
 
     time_elapsed = 0
@@ -612,7 +611,7 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
             if blabel == 'code':
                 code_output, rtime, fig_count = execute_script(bcontent,
                                                                example_globals,
-                                                               image_path,
+                                                               image_path_template,
                                                                fig_count,
                                                                src_file,
                                                                gallery_conf)
@@ -642,8 +641,8 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
                 example_rst += bcontent + '\n'
                 example_nb.add_markdown_cell(text2string(bcontent))
 
-    save_thumbnail(image_path, base_image_name, gallery_conf,
-                   thumb_num=thumbnail_number)
+    thumbnail_image_path = image_path_template.format(thumbnail_number)
+    save_thumbnail(thumbnail_image_path, base_image_name, gallery_conf)
 
     time_m, time_s = divmod(time_elapsed, 60)
     example_nb.save_file()
