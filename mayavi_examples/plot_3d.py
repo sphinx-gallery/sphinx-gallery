50d2bd320f8de00367c0b477fbee2e2a1f213884
Loïc Estève <loic.esteve@ymail.com>
2016-04-27 14:10:34 +0200

    Tweak mayavi example
    to highlight gotchas with ordering of matplotlib and mayavi figures
    within a cell.
    
    Also use sphinx_gallery_thumbnail_number to show how it is impacted by
    figure ordering.

--- mayavi_examples/plot_3d.py
+++ mayavi_examples/plot_3d.py
@@ -11,9 +11,25 @@ in order to test the autonomy of the gallery.
 # Code source: Alex Gramfort
 # License: BSD 3 clause
 
+# sphinx_gallery_thumbnail_number = 4
+
 from mayavi import mlab
 
 mlab.test_plot3d()
 
 mlab.figure()
 mlab.test_contour3d()
+
+#####################################################################
+# Note: this shows that inside a cell matplotlib figures are always
+# put before mayavi in the example HTML. In other words, the order of
+# plotting is not respected between matplotlib and mayavi figures
+
+mlab.figure()
+mlab.test_mesh()
+
+mlab.figure()
+mlab.test_flow()
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [1, 2, 3])
