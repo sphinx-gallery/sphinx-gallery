ece117d2f525f079d37e7dc000746a86f0043e21
Óscar Nájera <najera.oscar@gmail.com>
2016-06-27 00:12:46 +0200

    save_thumbnail finds image to display

--- sphinx_gallery/gen_rst.py
+++ sphinx_gallery/gen_rst.py
@@ -409,8 +409,13 @@ def scale_image(in_fname, out_fname, max_width, max_height):
                           generated images')
 
 
-def save_thumbnail(thumbnail_image_path, src_file, gallery_conf):
+def save_thumbnail(image_path_template, src_file, gallery_conf):
     """Save the thumbnail image"""
+    # read specification of the figure to display as thumbnail from main text
+    _, content = get_docstring_and_rest(src_file)
+    thumbnail_number = extract_thumbnail_number(content)
+    thumbnail_image_path = image_path_template.format(thumbnail_number)
+
     thumb_dir = os.path.join(os.path.dirname(thumbnail_image_path), 'thumb')
     if not os.path.exists(thumb_dir):
         os.makedirs(thumb_dir)
@@ -568,8 +573,13 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
     """
 
     src_file = os.path.join(src_dir, fname)
-    example_file = os.path.join(target_dir, fname)
-    shutil.copyfile(src_file, example_file)
+    script_blocks = split_code_and_text_blocks(src_file)
+    amount_of_code = sum([len(bcontent)
+                          for blabel, bcontent in script_blocks
+                          if blabel == 'code'])
+
+    if md5sum_is_current(src_file):
+        return amount_of_code, 0
 
     image_dir = os.path.join(target_dir, 'images')
     if not os.path.exists(image_dir):
@@ -579,21 +589,8 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
     image_fname = 'sphx_glr_' + base_image_name + '_{0:03}.png'
     image_path_template = os.path.join(image_dir, image_fname)
 
-    script_blocks = split_code_and_text_blocks(example_file)
-
-    # read specification of the figure to display as thumbnail from main text
-    _, content = get_docstring_and_rest(example_file)
-    thumbnail_number = extract_thumbnail_number(content)
-
-    amount_of_code = sum([len(bcontent)
-                          for blabel, bcontent in script_blocks
-                          if blabel == 'code'])
-
-    if md5sum_is_current(example_file):
-        return amount_of_code, 0
-
-    time_elapsed = 0
-
+    example_file = os.path.join(target_dir, fname)
+    shutil.copyfile(src_file, example_file)
     ref_fname = example_file.replace(os.path.sep, '_')
     example_rst = """\n\n.. _sphx_glr_{0}:\n\n""".format(ref_fname)
     example_nb = Notebook(fname, target_dir)
@@ -620,6 +617,7 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
     # A simple example has two blocks: one for the
     # example introduction/explanation and one for the code
     is_example_notebook_like = len(script_blocks) > 2
+    time_elapsed = 0
     for blabel, bcontent in script_blocks:
         if blabel == 'code':
             code_output, rtime = execute_codeblock(bcontent,
@@ -650,8 +648,7 @@ def generate_file_rst(fname, target_dir, src_dir, gallery_conf):
         with open(example_file + '.md5', 'w') as file_checksum:
             file_checksum.write(get_md5sum(example_file))
 
-    thumbnail_image_path = image_path_template.format(thumbnail_number)
-    save_thumbnail(thumbnail_image_path, src_file, gallery_conf)
+    save_thumbnail(image_path_template, src_file, gallery_conf)
 
     time_m, time_s = divmod(time_elapsed, 60)
     example_nb.save_file()
