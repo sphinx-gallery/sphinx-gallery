76bda4add441a4fd763f397e7fe1b0c62c14b693
Óscar Nájera <najera.oscar@gmail.com>
2016-07-03 20:31:13 +0200

    Explicit failed example order

--- examples/plot_raise.py
+++ examples/plot_raise.py
@@ -10,14 +10,6 @@ image stamp. Thus allowing easy identification in the gallery display.
 You also get the python traceback of the failed code block
 """
 
-iae
-
-###############################################################################
-# Sphinx gallery will stop executing the remaining code blocks after
-# the exception has occurred in the example script. Nevertheless the
-# html will still render all the example annotated text and
-# code blocks, but no output will be shown.
-
 # Code source: Óscar Nájera
 # License: BSD 3 clause
 
@@ -26,8 +18,18 @@ import matplotlib.pyplot as plt
 
 plt.pcolormesh(np.random.randn(100, 100))
 
+###############################################################################
+# This next block will raise a NameError
+
+iae
 
 ###############################################################################
-# Here is another error raising Block
+# Sphinx gallery will stop executing the remaining code blocks after
+# the exception has occurred in the example script. Nevertheless the
+# html will still render all the example annotated text and
+# code blocks, but no output will be shown.
+
+###############################################################################
+# Here is another error raising Block but will not be executed
 
 plt.plot('Strings are not a valid argument for the plot function')
