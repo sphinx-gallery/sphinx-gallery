498a548fbb08abf7416b0f3ecae87827b423d140
Óscar Nájera <najera.oscar@gmail.com>
2016-11-28 00:47:36 +0100

    Build gallery when example has SyntaxError
    As reported in #126 if an example has a SyntaxError gallery build
    breaks. This provides the necessary changes to continue build and
    raise an error at the end on the summary of errors

---
+++ examples/plot_syntaxerror.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Example with SyntaxError
+========================
+
+Sphinx-Gallery uses Python's AST parser, thus you need to have written
+valid python code for Sphinx-Gallery to parse it. If your script has a
+SyntaxError you'll be presented the traceback and the original code.
+
+"""
+# Code source: Óscar Nájera
+# License: BSD 3 clause
+
+Invalid Python code
