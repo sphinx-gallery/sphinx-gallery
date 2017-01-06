8c0128f2cba04d01bf3fb0631ea473c6ff4232d1
Loïc Estève <loic.esteve@ymail.com>
2017-01-06 12:16:30 +0100

    Fix warning about intersphinx inventory link

--- doc/conf.py
+++ doc/conf.py
@@ -292,7 +292,8 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'https://docs.python.org/{.major}'.format(sys.version_info): None}
 
 examples_dirs = ['../examples', '../tutorials']
 gallery_dirs = ['auto_examples', 'tutorials']
