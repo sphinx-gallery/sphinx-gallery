3f593d35440cd581cd59dd4dbcd8dc4462d1aece
Eric Larson <larson.eric.d@gmail.com>
2017-06-28 15:16:10 -0700

    Use unicode to read README.txt Gallery description (#256)

--- sphinx_gallery/gen_gallery.py
+++ sphinx_gallery/gen_gallery.py
@@ -11,6 +11,7 @@ when building the documentation.
 
 
 from __future__ import division, print_function, absolute_import
+import codecs
 import copy
 import re
 import os
@@ -188,24 +189,24 @@ def generate_gallery_rst(app):
         computation_times += this_computation_times
 
         # we create an index.rst with all examples
-        fhindex = open(os.path.join(gallery_dir, 'index.rst'), 'w')
-        # :orphan: to suppress "not included in TOCTREE" sphinx warnings
-        fhindex.write(":orphan:\n\n" + this_fhindex)
-        for directory in sorted(os.listdir(examples_dir)):
-            if os.path.isdir(os.path.join(examples_dir, directory)):
-                src_dir = os.path.join(examples_dir, directory)
-                target_dir = os.path.join(gallery_dir, directory)
-                this_fhindex, this_computation_times = generate_dir_rst(src_dir, target_dir, gallery_conf,
-                                                                        seen_backrefs)
-                fhindex.write(this_fhindex)
-                computation_times += this_computation_times
+        with codecs.open(os.path.join(gallery_dir, 'index.rst'), 'w',
+                         encoding='utf-8') as fhindex:
+            # :orphan: to suppress "not included in TOCTREE" sphinx warnings
+            fhindex.write((u":orphan:\n\n" + this_fhindex))
+            for directory in sorted(os.listdir(examples_dir)):
+                if os.path.isdir(os.path.join(examples_dir, directory)):
+                    src_dir = os.path.join(examples_dir, directory)
+                    target_dir = os.path.join(gallery_dir, directory)
+                    this_fhindex, this_computation_times = generate_dir_rst(src_dir, target_dir, gallery_conf,
+                                                                            seen_backrefs)
+                    fhindex.write(this_fhindex)
+                    computation_times += this_computation_times
 
-        if gallery_conf['download_all_examples']:
-            download_fhindex = generate_zipfiles(gallery_dir)
-            fhindex.write(download_fhindex)
+            if gallery_conf['download_all_examples']:
+                download_fhindex = generate_zipfiles(gallery_dir)
+                fhindex.write(download_fhindex)
 
-        fhindex.write(SPHX_GLR_SIG)
-        fhindex.flush()
+            fhindex.write(SPHX_GLR_SIG)
 
     if gallery_conf['plot_gallery']:
         logger.info("Computation time summary:", color='white')
--- sphinx_gallery/gen_rst.py
+++ sphinx_gallery/gen_rst.py
@@ -378,7 +378,8 @@ def generate_dir_rst(src_dir, target_dir, gallery_conf, seen_backrefs):
                        location=src_dir)
         return "", []  # because string is an expected return type
 
-    with open(os.path.join(src_dir, 'README.txt')) as fid:
+    with codecs.open(os.path.join(src_dir, 'README.txt'), 'r',
+                     encoding='utf-8') as fid:
         fhindex = fid.read()
     # Add empty lines to avoid bug in issue #165
     fhindex += "\n\n"
