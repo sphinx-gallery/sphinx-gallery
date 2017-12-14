94a5c9308f3ce7d1dca4312c0e69a31e8cfd2927
Óscar Nájera <hello@oscarnajera.com>
2017-12-15 00:58:50 +0100

    Install bokeh everywhere

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -18,10 +18,10 @@ if [ "$DISTRIB" == "conda" ]; then
     # force no mkl because mayavi requires old version of numpy
     # which then crashes with pandas and seaborn
     conda create --yes -n testenv python=$PYTHON_VERSION pip nomkl numpy\
-        setuptools matplotlib pillow pytest pytest-cov coverage seaborn
+        setuptools matplotlib pillow pytest pytest-cov coverage seaborn bokeh
     source activate testenv
     if [ "$INSTALL_MAYAVI" == "true" ]; then
-        conda install --yes mayavi
+        conda install --yes mayavi bokeh
     fi
     if [ "$SPHINX_VERSION" != "dev" ]; then
         conda install "sphinx=${SPHINX_VERSION-*}" --yes
@@ -38,7 +38,7 @@ elif [ "$DISTRIB" == "ubuntu" ]; then
     pip install -U requests[security]  # ensure SSL certificate works
     pip install "tornado<5"
     pip install -r requirements.txt
-    pip install seaborn sphinx==1.5.5 pytest "six>=1.10.0" pytest-cov
+    pip install seaborn sphinx==1.5.5 pytest "six>=1.10.0" pytest-cov bokeh
 else
     echo "invalid value for DISTRIB environment variable: $DISTRIB"
     exit 1
