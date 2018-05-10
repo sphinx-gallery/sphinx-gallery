8f0621229a52268782c674585b9b6ca01663fa6e
Antony Lee <anntzer.lee@gmail.com>
2018-05-10 15:21:00 -0700

    Backport exec to Py2.

--- sphinx_gallery/clean.py
+++ sphinx_gallery/clean.py
@@ -5,6 +5,23 @@ import subprocess
 import sys
 
 
+if sys.version_info < (3,):
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
+else:
+    import builtins
+    exec_ = builtins.exec
+
+
 def main():
     parser = ArgumentParser(
         description=
@@ -14,8 +31,8 @@ def main():
     parser.add_argument("builddir")
     args, _ = parser.parse_known_args()
     d = {}
-    with open(os.path.join(args.sourcedir, "conf.py")) as file:
-        exec(file.read(), d)
+    with open(os.path.join(args.sourcedir, "conf.py")) as conf_file:
+        exec_(conf_file.read(), d)
     sg_conf = d["sphinx_gallery_conf"]
     for path in sg_conf["gallery_dirs"]:
         shutil.rmtree(os.path.join(args.sourcedir, path), ignore_errors=True)
