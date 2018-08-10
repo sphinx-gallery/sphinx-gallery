58c2dccf9e6a06a2a51a1197d54af297d4cc565a
Óscar Nájera <hello@oscarnajera.com>
2018-08-10 19:26:46 +0200

    Fix required install [circle nomerge]

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -18,7 +18,8 @@ if [ "$DISTRIB" == "conda" ]; then
     # force no mkl because mayavi requires old version of numpy
     # which then crashes with pandas and seaborn
     conda create --yes -n testenv python=$PYTHON_VERSION pip nomkl numpy\
-        setuptools matplotlib pillow pytest pytest-cov coverage seaborn bokeh selenium phantomjs
+          setuptools matplotlib pillow pytest pytest-cov coverage seaborn bokeh selenium
+    conda install -c conda-forge phantomjs
     source activate testenv
     if [ "$INSTALL_MAYAVI" == "true" ]; then
         conda install --yes mayavi bokeh
