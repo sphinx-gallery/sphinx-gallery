a5e18f159ad0b3046d06e4f19d33a1328dffb997
Eric Larson <larson.eric.d@gmail.com>
2018-08-29 12:14:03 -0400

    FIX: ancient pytest

--- continuous_integration/travis/install.sh
+++ continuous_integration/travis/install.sh
@@ -38,7 +38,7 @@ elif [ "$DISTRIB" == "ubuntu" ]; then
     pip install -U requests[security]  # ensure SSL certificate works
     pip install "tornado<5"
     pip install -r requirements.txt
-    pip install seaborn sphinx==1.5.5 pytest "six>=1.10.0" pytest-cov
+    pip install seaborn sphinx==1.5.5 "pytest<3.7.3" "six>=1.10.0" pytest-cov
 else
     echo "invalid value for DISTRIB environment variable: $DISTRIB"
     exit 1
