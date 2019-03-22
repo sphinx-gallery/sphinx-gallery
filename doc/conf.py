2c71c69da77ed948f4417ac6b9e8293a7dd34fae
Óscar Nájera <hello@oscarnajera.com>
2019-03-22 21:55:35 +0100

    Move to html default theme for read the docs to take over

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
+
+def setup(app):
+    app.add_stylesheet('theme_override.css')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
