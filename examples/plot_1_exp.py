fc175cbaac3e6393ff6acfc114f6cf038ba1719f
Lucy Liu <jliu176@gmail.com>
2020-03-17 17:50:42 +0100

    del white space

--- examples/plot_1_exp.py
+++ examples/plot_1_exp.py
@@ -7,7 +7,7 @@ This example demonstrates how to import a local module and how images are
 stacked when two plots are created in one code block. The variable ``N`` from
 the example 'Local module' (file ``local_module.py``) is imported in the code
 below. Further, note that when there is only one code block in an example, the
-output appears before the code block. 
+output appears before the code block.
 """
 
 # Code source: Óscar Nájera
