c0614829019a73ab8f8cf077023b8eee73b162a0
Lucy Liu <jliu176@gmail.com>
2020-05-12 21:07:22 +0200

    fix lint

--- sphinx_gallery/gen_gallery.py
+++ sphinx_gallery/gen_gallery.py
@@ -420,7 +420,8 @@ def _format_for_writing(costs, path, kind='rst'):
             t = '%0.2f sec' % (cost[0][0],)
         m = '{0:.1f} MB'.format(cost[0][1])
         lines.append([name, t, m])
-    lens = [max(x) for x in zip(*[[len(l) for l in ll] for ll in lines])]
+    lens = [max(x) for x in zip(*[[len(item) for item in cost]
+                                  for cost in lines])]
     return lines, lens
 
 
