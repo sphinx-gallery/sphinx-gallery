f11144197dba02d40158c57d63f4ad3b347f2630
Emmanuelle Gouillart <emma@plot.ly>
2020-06-23 13:41:35 +0200

    Update examples/plot_9_plotly.py
    Co-authored-by: Lucy Liu <jliu176@gmail.com>

--- examples/plot_9_plotly.py
+++ examples/plot_9_plotly.py
@@ -14,7 +14,7 @@ following lines to select the appropriate plotly renderer::
     import plotly.io as pio
     pio.renderers.default = 'sphinx_gallery'
 
-**Optional**: the sphinx_gallery renderer of plotly will not generate png
+**Optional**: the ``sphinx_gallery`` renderer of plotly will not generate png
 thumbnails. For png thumbnails, you can use instead the ``sphinx_gallery_png``
 renderer, and add ``plotly.io._sg_scraper.plotly_sg_scraper`` to the list of
 :ref:`image_scrapers`. The scraper requires you to
