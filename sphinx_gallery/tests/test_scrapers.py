af95764336561f9f7e04f8cdfc49b2ff668c5ff3
Lucy Liu <jliu176@gmail.com>
2020-12-30 21:50:27 +1100

    import skip

--- sphinx_gallery/tests/test_scrapers.py
+++ sphinx_gallery/tests/test_scrapers.py
@@ -15,6 +15,8 @@ from sphinx_gallery.utils import _get_image
 @pytest.fixture(scope='function')
 def gallery_conf(tmpdir):
     """Sets up a test sphinx-gallery configuration"""
+    # Skip if numpy not installed
+    pytest.importorskip("numpy")
     gallery_conf = _complete_gallery_conf({}, str(tmpdir), True, False)
     gallery_conf.update(examples_dir=str(tmpdir), gallery_dir=str(tmpdir))
     return gallery_conf
