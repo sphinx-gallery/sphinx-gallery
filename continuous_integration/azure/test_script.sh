47e98ad0c89467ab97389f5bcdabcb8f4ae66a2d
Lucy Liu <jliu176@gmail.com>
2021-01-17 11:59:19 +1100

    mv travis files to azure

--- continuous_integration/azure/test_script.sh
+++ continuous_integration/azure/test_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# This script is meant to be called by the "Run test_script.sh" step defined in
-# azure-pipelines.yml.
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
 #
 # License: 3-clause BSD
 
