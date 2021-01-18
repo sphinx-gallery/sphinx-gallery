edb0e7985bcbb84f52773be816f12fd1768c7823
Lucy Liu <jliu176@gmail.com>
2021-01-18 21:59:36 +1100

    update header

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -1,10 +1,8 @@
-
-
 #!/bin/bash
-# This script is meant to be called by the "install" step defined in
-# .travis.yml. See http://docs.travis-ci.com/ for more details.
-# The behavior of the script is controlled by environment variabled defined
-# in the .travis.yml in the top level folder of the project.
+# This script is meant to be called by the "Run install.sh" step defined in
+# azure-pipelines.yml.
+# The behavior of the script is controlled by environment variables defined
+# in the azure-pipelines.yml in the top level folder of the project.
 #
 # License: 3-clause BSD
 
