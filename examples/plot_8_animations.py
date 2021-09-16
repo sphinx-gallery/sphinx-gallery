17746ef0ec2210953fa1cf87c95c3b1329886e40
David Stansby <dstansby@gmail.com>
2021-09-16 10:08:57 +0100

    Add link
    Co-authored-by: Eric Larson <larson.eric.d@gmail.com>

--- examples/plot_8_animations.py
+++ examples/plot_8_animations.py
@@ -5,7 +5,8 @@ Matplotlib animation support
 Show a Matplotlib animation, which should end up nicely embedded below.
 
 In order to enable support for animations ``'matplotlib_animations'``
-must be set to ``True`` in the sphinx gallery configuration.
+must be set to ``True`` in the sphinx gallery
+:ref:`configuration <image_scrapers>`.
 """
 
 import numpy as np
