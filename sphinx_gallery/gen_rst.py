9ec05cb62829c5775c199c72ebbb4acc9d7f61ca
Lucy Liu <lucy.liu@bom.gov.au>
2021-12-10 16:50:10 +1100

    check

--- sphinx_gallery/gen_rst.py
+++ sphinx_gallery/gen_rst.py
@@ -99,7 +99,6 @@ class _LoggingTee(object):
 
         self.logger.verbose("test log")
         for line in lines:
-            print('logging to verbose')
             self.logger.verbose('%s', line)
 
     def flush(self):
