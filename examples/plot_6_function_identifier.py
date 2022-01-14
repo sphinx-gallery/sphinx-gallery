ad6b24d14a61af320f1579d3db566fa0a8edf56f
Lucy Liu <lucy.liu@bom.gov.au>
2022-01-14 20:07:34 +1100

    minor reformatting

--- examples/plot_6_function_identifier.py
+++ examples/plot_6_function_identifier.py
@@ -3,10 +3,25 @@
 Identifying function names in a script
 ======================================
 
-This demonstrates how Sphinx-Gallery identifies the names of functions,
-methods, attributes and objects used and classes instantated in the script
-using the internal function ``identify_names``. It also determins to which module
-they belong. Identified names are turned into code or intersphinx references.
+This demonstrates how Sphinx-Gallery identifies when
+
+1. a function/method/attribute/object is used or class instantiated in a
+   code block
+2. a function/method/attribute/object/class is referred to using sphinx markup
+   in a text block.
+
+Sphinx-Gallery examines both the executed code itself, as well as the
+text blocks (such as this one, or the one below) for these references and
+identifies the module they belong to. This means that by writing
+:obj:`numpy.sin` and :obj:`numpy.exp` here, they will be identified even though
+they are not used in the code. This is useful in particular when functions
+return classes (meaning it is not explicitly instantiated) -- if you add them
+to the documented blocks of examples that use them, they will be added to
+backreferences.
+
+This functionality is used to add documentation hyperlinks to your code
+(:ref:`link_to_documentation`) and for mini-gallery creation
+(:ref:`references_to_examples`).
 """
 
 # Code source: Óscar Nájera
@@ -28,16 +43,10 @@ _, script_blocks = split_code_and_text_blocks(filename)
 names = identify_names(script_blocks)
 
 # %%
-# Sphinx-Gallery examines both the executed code itself, as well as the
-# documentation blocks (such as this one, or the top-level one),
-# to find backreferences. This means that by writing :obj:`numpy.sin`
-# and :obj:`numpy.exp` here, backreferences will be created for them even
-# though they are not used in the code. This is useful in particular when
-# functions return classes -- if you add them to the documented blocks of
-# examples that use them, they will be shown in the backreferences.
-#
-# The figure below shows the functions as written in the code block on the
-# left and the full resolved import path for that function on the right.
+# In the code block above, we use the internal function ``identify_names`` to
+# obtain all identified names from this file and their full resolved import
+# path. We then plot this below, where the identified names functions are
+# on the left and the full resolved import path is on the right.
 
 fontsize = 12.5
 figheight = 3 * len(names) * fontsize / 72
