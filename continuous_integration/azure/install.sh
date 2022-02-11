6bc1bbd4bea0945042495d19878ab042a2d185f4
Eric Larson <larson.eric.d@gmail.com>
2022-02-11 15:42:34 -0500

    FIX: Azure (#915)
    * FIX: Azure
    
    * FIX: Install
    
    * FIX: Windows dep

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -19,7 +19,7 @@ if [ "$DISTRIB" == "conda" ]; then
     PIP_DEPENDENCIES="$@"
     PIP_DEPENDENCIES="$PIP_DEPENDENCIES sphinx_rtd_theme check-manifest"
     if [ "$PYTHON_VERSION" != "3.7" -o "$LOCALE" != "C" ]; then
-        PIP_DEPENDENCIES="${PIP_DEPENDENCIES} memory_profiler vtk<=9.0.1 traits<6.3.0 https://github.com/enthought/mayavi/zipball/master ipython pypandoc"
+        CONDA_TO_INSTALL="$CONDA_TO_INSTALL mayavi memory_profiler ipython pypandoc"
     fi
     if [ "$SPHINX_VERSION" == "" ]; then
         PIP_DEPENDENCIES="${PIP_DEPENDENCIES} sphinx"
@@ -29,7 +29,7 @@ if [ "$DISTRIB" == "conda" ]; then
         PIP_DEPENDENCIES="${PIP_DEPENDENCIES} sphinx==${SPHINX_VERSION}"
     fi
     source activate base
-    conda install --yes $CONDA_TO_INSTALL
+    conda install --yes -c conda-forge $CONDA_TO_INSTALL
     conda info --envs
     pytest --version
     python -m pip install $PIP_DEPENDENCIES
