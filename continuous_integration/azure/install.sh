ddaefcfac734112d0e9361b7059bb2f41660b070
Andreas Mueller <amueller@microsoft.com>
2022-07-17 10:42:29 -0500

    try to fix dependencies again

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -17,7 +17,7 @@ if [ "$DISTRIB" == "conda" ]; then
     CONDA_TO_INSTALL="$@"
     CONDA_TO_INSTALL="$CONDA_TO_INSTALL python=$PYTHON_VERSION pip numpy setuptools matplotlib pillow pytest pytest-cov coverage seaborn statsmodels 'plotly>=4.0' joblib flake8 wheel libiconv"
     PIP_DEPENDENCIES="$@"
-    PIP_DEPENDENCIES="$PIP_DEPENDENCIES sphinx_rtd_theme check-manifest"
+    PIP_DEPENDENCIES="$PIP_DEPENDENCIES sphinx_rtd_theme check-manifest jupyterlite-sphinx"
     if [ "$PYTHON_VERSION" != "3.7" -o "$LOCALE" != "C" ]; then
         CONDA_TO_INSTALL="$CONDA_TO_INSTALL mayavi memory_profiler ipython pypandoc"
     fi
