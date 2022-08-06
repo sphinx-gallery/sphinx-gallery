edf8a01593891fd14666aea774911eeb29732922
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2022-08-06 16:46:56 -0400

    Use Mock more in tests (#986)
    * Replace FakeSphinxApp with a Mock
    
    No need to re-implement something that's in the standard library.
    
    * Use a Mock in gallery_conf fixture
    
    This also drops the last use of `Bunch`.

--- sphinx_gallery/utils.py
+++ sphinx_gallery/utils.py
@@ -165,14 +165,6 @@ def _replace_md5(fname_new, fname_old=None, method='move', mode='b'):
     assert os.path.isfile(fname_old)
 
 
-class Bunch(dict):
-    """Dictionary-like object that exposes its keys as attributes."""
-
-    def __init__(self, **kwargs):  # noqa: D102
-        dict.__init__(self, kwargs)
-        self.__dict__ = self
-
-
 def _has_pypandoc():
     """Check if pypandoc package available."""
     try:
