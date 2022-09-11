0ec15043a67b906fbbfc6bfec502d6878072fe02
Kalai Ramea <kramea@parc.com>
2022-09-11 10:33:05 -0700

    clean code

---
+++ examples_old/local_module.py
@@ -0,0 +1,10 @@
+"""
+Local module
+============
+
+This example demonstrates how local modules can be imported.
+This module is imported in the example 'Plotting the exponential function'
+(``plot_exp.py``).
+"""
+
+N = 100
