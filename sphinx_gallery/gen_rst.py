a72a0ce76a73b3b4924a3a393497db70407d4890
Alexis Thual <alexisthual@gmail.com>
2022-09-21 18:58:45 +0200

    Revert "Sanitize thumbnail title"
    This reverts commit 09ced42c5c7593e9933bd9589c5b6e6b6785ad24.

--- sphinx_gallery/gen_rst.py
+++ sphinx_gallery/gen_rst.py
@@ -208,18 +208,6 @@ def _sanitize_rst(string):
     string = re.sub(p + r'`([^`]+)`' + e, r'\1\2\3', string)
     # `whatever thing` --> whatever thing
     string = re.sub(p + r'([^`]+)' + e, r'\1\2\3', string)
-
-    # **string** --> string
-    string = re.sub(r'\*\*([^\*]*)\*\*', r'\1', string)
-    # *string* --> string
-    string = re.sub(r'\*([^\*]*)\*', r'\1', string)
-    # `link text <url>`_ --> link text
-    string = re.sub(r'`(.*) <.*>`\_', r'\1', string)
-    # :term:`the term` --> the term
-    string = re.sub(r':term:`(.*)`', r'\1', string)
-    # :ref:`the ref` --> the ref
-    string = re.sub(r':ref:`(.*)`', r'\1', string)
-
     return string
 
 
@@ -254,9 +242,6 @@ def extract_intro_and_title(filename, docstring):
     intro = _sanitize_rst(intro)
     if len(intro) > 95:
         intro = intro[:95] + '...'
-
-    title = _sanitize_rst(title)
-
     return intro, title
 
 
