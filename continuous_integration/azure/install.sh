f2e2ca2ea1bf087ffe99d2442a148e4b127e9302
Eric Larson <larson.eric.d@gmail.com>
2023-01-23 17:19:36 -0500

    FIX: Try again

--- continuous_integration/azure/install.sh
+++ continuous_integration/azure/install.sh
@@ -26,8 +26,7 @@ if [ "$DISTRIB" == "conda" ]; then
     elif [ "$SPHINX_VERSION" == "dev" ]; then
         # It is a mystery to me why we need black, but we get an error with sphinx that it's needed at the end of the build...
         # TODO: Can't use dev until pydata-sphinx-theme supports it!
-        PIP_DEPENDENCIES="${PIP_DEPENDENCIES} black sphinx<6"  # https://api.github.com/repos/sphinx-doc/sphinx/zipball/master
-        CONDA_TO_INSTALL="$CONDA_TO_INSTALL libiconv"
+        PIP_DEPENDENCIES="${PIP_DEPENDENCIES} black 'sphinx<6'"  # https://api.github.com/repos/sphinx-doc/sphinx/zipball/master
     else
         PIP_DEPENDENCIES="${PIP_DEPENDENCIES} sphinx==${SPHINX_VERSION} jinja2<=3.0.3"
     fi
