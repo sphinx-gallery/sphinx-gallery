87ebbb92584d8099b97eb23a1b759943af89a585
Lucy Liu <jliu176@gmail.com>
2023-08-18 12:44:47 +1000

    amend wordingg

--- sphinx_gallery/tests/tinybuild/examples/plot_repr.py
+++ sphinx_gallery/tests/tinybuild/examples/plot_repr.py
@@ -7,7 +7,7 @@ Test repr and the sphinx_gallery_dummy_images config.
 
 # sphinx_gallery_dummy_images=2
 class A:
-    """Test class with `_repr_html_` method."""
+    """Class with `_repr_html_` method for testing."""
 
     def _repr_html_(self):
         return "<p><b>This should print<b></p>"
