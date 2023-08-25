a06f0262252e7bc063f917f78fcc851b20c25603
ArturoAmorQ <arturo.amor-quiroz@polytechnique.edu>
2023-08-25 14:40:31 +0200

    Simplify use of default values

--- sphinx_gallery/gen_gallery.py
+++ sphinx_gallery/gen_gallery.py
@@ -82,7 +82,7 @@ DEFAULT_GALLERY_CONF = {
     "download_all_examples": True,
     "abort_on_example_error": False,
     "only_warn_on_example_error": False,
-    "recommender": {"enable": False, "n_examples": 5, "min_df": 1, "max_df": 1.0},
+    "recommender": {"enable": False},
     "failing_examples": {},
     "passing_examples": [],
     "stale_examples": [],  # ones that did not need to be run due to md5sum
@@ -666,9 +666,9 @@ def generate_gallery_rst(app):
 
         # Build recommendation system
         if gallery_conf["recommender"]["enable"]:
-            n_examples = gallery_conf["recommender"]["n_examples"]
-            min_df = gallery_conf["recommender"]["min_df"]
-            recommender = ExampleRecommender(n_examples=n_examples, min_df=min_df)
+            recommender_params = copy.deepcopy(gallery_conf["recommender"])
+            recommender_params.pop("enable")
+            recommender = ExampleRecommender(**recommender_params)
 
             gallery_py_files = []
             # root and subsection directories containing python examples
