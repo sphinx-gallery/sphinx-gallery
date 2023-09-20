7122f76125747d80f58f187427af513f8651e5c9
Eric Larson <larson.eric.d@gmail.com>
2023-09-20 10:19:22 -0400

    FIX: Reg

--- sphinx_gallery/tests/test_full.py
+++ sphinx_gallery/tests/test_full.py
@@ -872,7 +872,7 @@ def _rerun(
     # - auto_examples/index
     # - auto_examples/plot_numpy_matplotlib
     if how == "modify":
-        n_ch = "[3-10]"
+        n_ch = "([3-9]|10)"
     else:
         n_ch = "[1-8]"
     lines = "\n".join([f"\n{how} != {n_ch}:"] + lines)
