1c9ba2af8d1b9135d2109c32c302df236879aac0
Eric Larson <larson.eric.d@gmail.com>
2023-10-24 11:13:58 -0400

    FIX: Again [skip actions]

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -8,7 +8,6 @@ sudo apt --no-install-recommends install -yq \
 python3 -m venv ~/python_env
 echo "set -e" >> "$BASH_ENV"
 echo "source ~/python_env/bin/activate" >> "$BASH_ENV"
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1400x900x24 -ac +extension GLX +render -noreset
 source ~/python_env/bin/activate
 echo "Python: $(which python)"
 echo "pip:    $(which pip)"
