34964aa821a827fbd6cd98f407c58e6453ba7a20
Eric Larson <larson.eric.d@gmail.com>
2023-10-24 11:27:56 -0400

    FIX: PyVista [skip actions]

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 set -eo pipefail
+echo "set -eo pipefail" >> "$BASH_ENV"
 sudo apt update
 sudo apt --no-install-recommends install -yq \
     libgl1 libosmesa6 mesa-utils \
     optipng graphviz python3-venv
 python3 -m venv ~/python_env
-echo "set -e" >> "$BASH_ENV"
-echo "source ~/python_env/bin/activate" >> "$BASH_ENV"
 source ~/python_env/bin/activate
+echo "source ~/python_env/bin/activate" >> "$BASH_ENV"
 echo "Python: $(which python)"
 echo "pip:    $(which pip)"
