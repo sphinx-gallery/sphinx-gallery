d051cdaeed75749298eb74bc40c2e81dae995bbc
Eric Larson <larson.eric.d@gmail.com>
2023-10-24 11:20:26 -0400

    FIX: Fine [skip actions][

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -3,8 +3,8 @@
 set -eo pipefail
 sudo apt update
 sudo apt --no-install-recommends install -yq \
-    libgl1 libosmesa6 mesa-utils \  # 3D
-    optipng graphviz python3-venv  # sphinx-gallery
+    libgl1 libosmesa6 mesa-utils \
+    optipng graphviz python3-venv
 python3 -m venv ~/python_env
 echo "set -e" >> "$BASH_ENV"
 echo "source ~/python_env/bin/activate" >> "$BASH_ENV"
