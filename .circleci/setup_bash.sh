2e01f6043268819b90a1f17605e4855e1fc8e048
Eric Larson <larson.eric.d@gmail.com>
2023-10-24 11:15:43 -0400

    FIX: Quarantine [skip actions]

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -2,9 +2,7 @@
 
 set -eo pipefail
 sudo apt update
-sudo apt --no-install-recommends install -yq \
-    texlive texlive-latex-extra latexmk optipng tex-gyre graphviz \
-    python3-venv
+sudo apt --no-install-recommends install -yq optipng graphviz python3-venv
 python3 -m venv ~/python_env
 echo "set -e" >> "$BASH_ENV"
 echo "source ~/python_env/bin/activate" >> "$BASH_ENV"
