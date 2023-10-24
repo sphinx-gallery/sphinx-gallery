9d32cc574499f124e0e95799a9bfad5e2fad525d
Eric Larson <larson.eric.d@gmail.com>
2023-10-24 11:19:46 -0400

    FIX: Try simpler [skip actions]

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 sudo apt update
 sudo apt --no-install-recommends install -yq \
-    libopengl0 libegl1 libosmesa6 mesa-utils \  # 3D
+    libgl1 libosmesa6 mesa-utils \  # 3D
     optipng graphviz python3-venv  # sphinx-gallery
 python3 -m venv ~/python_env
 echo "set -e" >> "$BASH_ENV"
