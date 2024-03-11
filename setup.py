d9b725c8eceab7e55d01e86c4a886fb81d85c635
Jirka <jirka.borovec@seznam.cz>
2024-03-11 18:55:32 +0100

    [project.scripts] sphinx_gallery_py2jupyter

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Author: Óscar Nájera
-# License: 3-clause BSD
-"""Install Sphinx extension for gallery generator."""
-
-from setuptools import setup
-
-setup(
-    scripts=["bin/copy_sphinxgallery.sh", "bin/sphx_glr_python_to_jupyter.py"],
-)
