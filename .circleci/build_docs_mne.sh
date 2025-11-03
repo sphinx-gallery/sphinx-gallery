2ec93a45bc9d392e7dd028bc838eb6fe270a147b
Eric Larson <larson.eric.d@gmail.com>
2025-11-03 16:37:40 -0500

    FIX: No verbose needed

--- .circleci/build_docs_mne.sh
+++ .circleci/build_docs_mne.sh
@@ -17,7 +17,7 @@ __sep__
 git clone git@github.com:/mne-tools/mne-python.git
 pushd mne-python
 git checkout -b credit origin/credit  # TODO: REMOVE BEFORE MERGE, NEEDS https://github.com/mne-tools/mne-python/pull/13477!
-pip install -ve . --group doc mne-qt-browser "PySide6!=6.10.0"
+pip install -e . --group doc mne-qt-browser "PySide6!=6.10.0"
 popd
 
 __sep__
