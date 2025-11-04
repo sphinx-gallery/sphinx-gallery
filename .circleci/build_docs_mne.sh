74efde4421c03d77b0755e85d50d23c617e369d9
Eric Larson <larson.eric.d@gmail.com>
2025-11-04 08:50:46 -0500

    FIX: No need for branch

--- .circleci/build_docs_mne.sh
+++ .circleci/build_docs_mne.sh
@@ -15,10 +15,8 @@ set -exo pipefail
 __sep__
 
 git clone git@github.com:/mne-tools/mne-python.git
-pushd mne-python
-git checkout -b credit origin/credit  # TODO: REMOVE BEFORE MERGE, NEEDS https://github.com/mne-tools/mne-python/pull/13477!
+cd mne-python
 pip install -e . --group doc mne-qt-browser "PySide6!=6.10.0"
-popd
 
 __sep__
 
@@ -26,11 +24,11 @@ mne sys_info -d
 
 __sep__
 
-./.circleci/sg_dev_check.sh
+../.circleci/sg_dev_check.sh
 
 __sep__
 
 export MNE_BROWSER_BACKEND=qt
 export MNE_BROWSER_PRECOMPUTE=false
 export PATTERN=10_array_objs
-make -C mne-python/doc html-pattern
+make -C doc html-pattern
--- .circleci/sg_dev_check.sh
+++ .circleci/sg_dev_check.sh
@@ -2,6 +2,9 @@
 
 # Ensure we are using local version of SG
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SG_SRC_DIR=$(dirname "$SCRIPT_DIR")/sphinx_gallery
+
 set -xeo pipefail
-SG_LOC=$(python -c "import sphinx_gallery, pathlib; print(pathlib.Path(sphinx_gallery.__file__).parent)")
-test "$SG_LOC" == "$(pwd)/sphinx_gallery"
+SG_IMPORT_LOC=$(python -c "import sphinx_gallery, pathlib; print(pathlib.Path(sphinx_gallery.__file__).parent)")
+test "$SG_IMPORT_LOC" == "$SG_SRC_DIR"
