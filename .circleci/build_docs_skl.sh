a4f7371ac1df6342ed152e19e2e328fad39f8c56
Lucy Liu <jliu176@gmail.com>
2025-11-28 12:56:09 +1100

    format

--- .circleci/build_docs_skl.sh
+++ .circleci/build_docs_skl.sh
@@ -8,8 +8,11 @@ function __sep__ () {
     printf %"$COLUMNS"s | tr " " "-"
     echo -e "\n"
 }
+
 set -exo pipefail
+
 (set +x; __sep__)
+
 git clone git@github.com:scikit-learn/scikit-learn.git
 cd scikit-learn
 conda create -n sklearn-dev -c conda-forge \
@@ -17,8 +20,12 @@ conda create -n sklearn-dev -c conda-forge \
   pytest pytest-cov ruff==0.11.2 mypy numpydoc \
   joblib threadpoolctl
 pip install -e .
+
 (set +x; __sep__)
+
 ../.circleci/sg_dev_check.sh
+
 (set +x; __sep__)
+
 cd doc
 make html -D sphinx_gallery_conf.filename_pattern=plot_grid_search_text_feature_extraction\|plot_display_object_visualization -D sphinx_gallery_conf.run_stale_examples=True
