80d3dc25fe95137bdf35ba6fa78b4c2d6150155c
Lucy Liu <jliu176@gmail.com>
2025-11-28 14:55:35 +1100

    try again

--- .circleci/build_docs_skl.sh
+++ .circleci/build_docs_skl.sh
@@ -15,6 +15,19 @@ set -exo pipefail
 
 git clone git@github.com:scikit-learn/scikit-learn.git
 cd scikit-learn
+
+# Deactivate python venv activated in `setup_bash.sh`
+deactivate
+
+# Install miniforge
+# Copied from: https://github.com/scikit-learn/scikit-learn/blob/94f18cefbdc145a9ae439112d7fc89d84467c647/build_tools/circle/build_doc.sh#L171-L176
+MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+curl -L --retry 10 $MINIFORGE_URL -o miniconda.sh
+MINIFORGE_PATH=$HOME/miniforge3
+bash ./miniconda.sh -b -p $MINIFORGE_PATH
+source $MINIFORGE_PATH/etc/profile.d/conda.sh
+conda activate
+
 conda create -n sklearn-dev -c conda-forge \
   python numpy scipy cython meson-python ninja \
   pytest pytest-cov ruff==0.11.2 mypy numpydoc \
