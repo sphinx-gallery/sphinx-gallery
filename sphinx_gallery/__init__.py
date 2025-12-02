92c17929b37c330c2fa5716eb1783b8297cccdb8
Eric Larson <larson.eric.d@gmail.com>
2025-12-02 10:46:22 -0500

    MAINT: Bump version

--- sphinx_gallery/__init__.py
+++ sphinx_gallery/__init__.py
@@ -8,7 +8,7 @@ import os
 
 # dev versions should have "dev" in them, stable should not.
 # doc/conf.py makes use of this to set the version drop-down.
-__version__ = "0.20.dev0"
+__version__ = "0.20.0"
 
 
 def glr_path_static():
