b9915e107f2ce3d9c0fcd0910c397e629199c7d7
Eric Larson <larson.eric.d@gmail.com>
2026-03-24 09:14:00 -0400

    FIX: More

--- .circleci/setup_bash.sh
+++ .circleci/setup_bash.sh
@@ -4,7 +4,8 @@ set -eo pipefail
 echo "set -eo pipefail" >> "$BASH_ENV"
 sudo apt update
 sudo apt --no-install-recommends install -yq ffmpeg graphviz optipng python3-venv \
-    xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libopengl0 libegl1 libosmesa6 mesa-utils libxcb-shape0 libxcb-cursor0 libxml2
+    xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libopengl0 libegl1 libosmesa6 mesa-utils libxcb-shape0 libxcb-cursor0 libxml2 \
+    r-base
 
 python3 -m venv ~/python_env
 source ~/python_env/bin/activate
