02fa40a28eab732104f55c47da477d6f51ce91b8
Eric Larson <larson.eric.d@gmail.com>
2026-06-22 22:26:24 -0400

    Fix docs and reqs for API usage

--- .github/install.sh
+++ .github/install.sh
@@ -46,12 +46,6 @@ pip install $EXTRA_ARGS $PIP_DEPENDENCIES \
     -e .${DEP_OPT}
 set +x
 
-# "pip install pygraphviz" does not guarantee graphviz binaries exist
-if [[ "$DISTRIB" != "mamba" ]]; then
-    if [[ "$PLATFORM" == "Linux" ]]; then
-        sudo apt install ffmpeg graphviz
-    else  # could use brew on macOS pip but it'll take time to install
-        echo "Removing pygraphviz on $PLATFORM when DISTRIB=$DISTRIB"
-        pip uninstall -y graphviz
-    fi
+if [[ "$DISTRIB" != "mamba" ]] && [[ "$PLATFORM" == "Linux" ]]; then
+    sudo apt install ffmpeg graphviz
 fi
