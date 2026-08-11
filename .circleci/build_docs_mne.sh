26eacc1cfb1ec6a7910de72cdc1f0621fcb094d5
Eric Larson <larson.eric.d@gmail.com>
2026-08-10 22:51:16 -0400

    Revert "TEMP: Timestamp the MNE doc build log [actions skip]"
    This reverts commit 063616dad6423a6e8e2860c0b258e305fd89b38d.

--- .circleci/build_docs_mne.sh
+++ .circleci/build_docs_mne.sh
@@ -31,11 +31,4 @@ mne sys_info -d
 export MNE_BROWSER_BACKEND=qt
 export MNE_BROWSER_PRECOMPUTE=false
 export PATTERN=10_array_objs
-# TEMPORARY: prefix every line with elapsed time so we can see which build
-# phase this job actually spends its ~14 min in. Revert once we know.
-make -C doc html-pattern 2>&1 | python -u -c "
-import sys, time
-t0 = time.time()
-for line in sys.stdin:
-    sys.stdout.write(f'[{time.time() - t0:8.1f}s] {line}')
-"
+make -C doc html-pattern
